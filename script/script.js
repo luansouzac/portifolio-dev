382904487d543d0ea6b308c98d0e4b87c662bf41
Luan Souza <luansouzac14@gmail.com>
2025-07-05 12:33:09 -0300

    feat: one single page

--- script/script.js
+++ script/script.js
@@ -7,3 +7,27 @@ window.addEventListener("scroll", () => {
     const line = document.getElementById("scroll-line");
     line.style.height = `${scrollPercent * 100}vh`;
 });
+
+function mostrarSecao(secaoAlvoClasse) {
+  const secoes = document.querySelectorAll('main section');
+
+  secoes.forEach(secao => {
+    if (secao.classList.contains(secaoAlvoClasse)) {
+      secao.style.display = 'flex'; 
+    } else {
+      secao.style.display = 'none';
+    }
+  });
+}
+
+function home() {
+  mostrarSecao('home');
+}
+
+function projetos() {
+  mostrarSecao('projetos');
+}
+
+function contato() {
+  mostrarSecao('contato');
+}
