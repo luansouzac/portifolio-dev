da79d42d3b734e523eb5cf22677ee8bbaacdd934
luansouzac <luansouzac14@gmail.com>
2025-07-21 19:56:22 -0300

    feat: Nova section de tecnologias

--- script/script.js
+++ script/script.js
@@ -1,13 +1,3 @@
-
-window.addEventListener("scroll", () => {
-    const scrollTop = window.scrollY;
-    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
-    const scrollPercent = scrollTop / docHeight;
-
-    const line = document.getElementById("scroll-line");
-    line.style.height = `${scrollPercent * 100}vh`;
-});
-
 function mostrarSecao(secaoAlvoClasse) {
   const secoes = document.querySelectorAll('main section');
 
@@ -32,41 +22,10 @@ function contato() {
   mostrarSecao('contato');
 }
 
-let front = document.getElementById("front");
-let back = document.getElementById("back");
+function tecnologias() {
+  mostrarSecao('tecnologias');
+}
 
-let containerFront = document.getElementById("container-front");
-let containerBack = document.getElementById("container-back");
-
-front.addEventListener("mouseover", function () {
-  containerFront.style.opacity = "1";
-  containerFront.style.visibility = "visible";
-  containerFront.style.transform = "translateY(0)";
-});
-
-front.addEventListener("mouseout", function () {
-  containerFront.style.opacity = "0";
-  containerFront.style.transform = "translateY(10px)";
-
-  setTimeout(() => {
-    containerFront.style.visibility = "hidden";
-  }, 300);
-});
-
-back.addEventListener("mouseover", function () {
-  containerBack.style.opacity = "1";
-  containerBack.style.visibility = "visible";
-  containerBack.style.transform = "translateY(0)";
-});
-
-back.addEventListener("mouseout", function () {
-  containerBack.style.opacity = "0";
-  containerBack.style.transform = "translateY(10px)";
-
-  setTimeout(() => {
-    containerBack.style.visibility = "hidden";
-  }, 300);
-});
 
 
 
